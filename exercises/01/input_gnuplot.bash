dcefa7ef868eac3ea7e153536ba9e303c085d3f6
Thiago Genez <thiagogenez@ic.unicamp.br>
2012-10-29 14:55:22 -0200

    fix gnuplot script

--- exercises/01/input_gnuplot.bash
+++ exercises/01/input_gnuplot.bash
@@ -3,7 +3,7 @@
 
 SERVICE_RATES="100 95 90 85 80 75 70 65 60 55 50 45 40 35 30 25 20 15 14 13 12 11 10"
 OUTPUT_PATH="/local2/thiagogenez/nqs/01/results"
-DATA="Utilization Mean-delay Mean-queue-length"
+DATA=('Utilization' 'Mean-delay' 'Mean-queue-length')
 
 if [ -d $OUTPUT_PATH/gnuplot ]; then
 	rm -rf $OUTPUT_PATH/gnuplot
@@ -14,7 +14,7 @@ mkdir $OUTPUT_PATH/gnuplot
 for rate in $SERVICE_RATES; do
 	array=()
 	i=0
-	for data in ; do
+	for data in ${DATA[@]}; do
 		array[i]=`cat $OUTPUT_PATH/*serviceRate-$rate-Mbit* | grep "$data" | awk -F " " {'print $5'} | awk '{sum+=$1; sumsq+=$1*$1;} END {avg=sum/NR; std=sqrt(sumsq/NR - avg^2); ci95=(1.96 * std)/sqrt(NR); printf "%.12f %.12f %.12f %.12f %.12f\n", avg, std, ci95, avg-ci95, avg+ci95}' | awk '{print $1, $3}'` 
 		((i++))
 	done
