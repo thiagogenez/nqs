bf4d659f2fcb5933032582d3d9c6e25cb5fe3baf
Thiago Genez <thiagogenez@lrc.ic.unicamp.br>
2012-10-29 16:45:41 -0200

    fix some bugs

--- exercises/01/plot.bash
+++ exercises/01/plot.bash
@@ -27,9 +27,9 @@ for data in ${DATA[@]}; do
 
 		set key left top
 		set term postscript enhanced eps dashed lw 1 "Helvetica" 14
-		set output "`echo $data | tr "[:upper:]" "[:lower:]"`.plot "
+		set output "`echo $data | tr "[:upper:]" "[:lower:]"`.eps"
 
-		plot  $OUTPUT_PATH/gnuplot/`echo $data | tr "[:upper:]" "[:lower:]"`.plot  using 1:2 title "Mean Delay" with linespoints
+		plot "$OUTPUT_PATH/gnuplot/`echo $data | tr "[:upper:]" "[:lower:]"`.plot"  using 1:2:3 title "Mean Delay" with yerrorlines pointtype 12
 EOF
 
 done
